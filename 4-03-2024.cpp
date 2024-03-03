cb9eb34e8b08d11f64f5dc2932f52ba0ca1aa019
Ekansh Jain <161363934+Ekansh-CSE@users.noreply.github.com>
2024-03-04 00:13:46 +0530

    Create 4-03-2024.cpp

---
+++ 4-03-2024.cpp
@@ -0,0 +1,12 @@
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        int i=0;
+        while(i+2<n){
+            swap(arr[i],arr[i+2]);
+            i++;
+        }
+        
+    }
+};
