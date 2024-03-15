409c6a84ed5f5682d61e1e1a8074c88136dd8485
Ekansh Jain <161363934+Ekansh-CSE@users.noreply.github.com>
2024-03-16 00:08:20 +0530

    Create 16-03-2024.cpp

---
+++ 16-03-2024.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+    public:
+   void deleteNode(Node *del_node)
+{
+         del_node->data = del_node->next->data;
+           del_node->next = del_node->next->next;
+    }
+
+};
